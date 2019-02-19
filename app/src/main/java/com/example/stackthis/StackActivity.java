2720efd1c147f6591feee84d054b9c753c190ed0
szb18158 <szb18158@uni.strath.ac.uk>
2019-02-19 15:29:53 +0000

    15:29

--- app/src/main/java/com/example/stackthis/StackActivity.java
+++ app/src/main/java/com/example/stackthis/StackActivity.java
@@ -11,10 +11,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class StackActivity extends AppCompatActivity {
 
     private ListView myListView;
+    private Stack stack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +28,18 @@ public class StackActivity extends AppCompatActivity {
         //Store data field object.
         myListView = (ListView)findViewById(R.id.my_list_view);
 
-        //An arraylist that contains the items to be displayed.
-        ArrayList<String> myListItems = new ArrayList<String>();
-        myListItems.add("Hello");
-        myListItems.add("world");
-        myListItems.add("!!");
+//        //An arraylist that contains the items to be displayed.
+//        ArrayList<String> myListItems = new ArrayList<String>();
+//        myListItems.add("Hello");
+//        myListItems.add("world");
+//        myListItems.add("!!");
+
+
+        stack = new Stack();
+        stack.push(5);
 
         //Create an arrayadapter. The listview needs this.
-        final ArrayAdapter myArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, myListItems);
+        final ArrayAdapter myArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stack);
 
         //Set the adapter for the listview.
         myListView.setAdapter(myArrayAdapter);
