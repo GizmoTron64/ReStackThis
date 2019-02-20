bb790fa71d2a2a7cc592b4fde4ceab52289aadf5
GizmoTron64 <patrick_gray64@rocketmail.com>
2019-02-20 00:34:09 +0000

    12.33

--- app/src/main/java/com/example/stackthis/StackActivity.java
+++ app/src/main/java/com/example/stackthis/StackActivity.java
@@ -1,22 +1,24 @@
 package com.example.stackthis;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class StackActivity extends AppCompatActivity {
 
-    private ListView myListView;
+    private ListView stackListView;
     private Stack stack;
+    private EditText enterVal;
+    private TextView topEditText;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,33 +27,65 @@ public class StackActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar3);
         setSupportActionBar(toolbar);
 
-        //Store data field object.
-        myListView = (ListView)findViewById(R.id.my_list_view);
-
-//        //An arraylist that contains the items to be displayed.
-//        ArrayList<String> myListItems = new ArrayList<String>();
-//        myListItems.add("Hello");
-//        myListItems.add("world");
-//        myListItems.add("!!");
-
-
         stack = new Stack();
-        stack.push(5);
-
-        //Create an arrayadapter. The listview needs this.
-        final ArrayAdapter myArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stack);
-
-        //Set the adapter for the listview.
-        myListView.setAdapter(myArrayAdapter);
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stack);
+        stackListView = (ListView) findViewById(R.id.stackListView);
+        stackListView.setAdapter(listAdapter);
 
         //Set an event listener: what happens when an item on the listview is clicked.
-        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        stackListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-
+                listAdapter.remove(listAdapter.getItem(position));
+                topVal(view);
             }
         });
+
+
+    }
+
+    /**
+     * Adds a value to the end of the stack
+     * @param view
+     */
+    public void pushVal(View view){
+        enterVal = (EditText) findViewById(R.id.enterVal);
+        String message = enterVal.getText().toString();
+        listAdapter.add(message);
+        enterVal.getText().clear();
+        topVal(view);
+    }
+
+    /**
+     * Removes the last value in the stack
+     * @param view
+     */
+    public void popVal(View view) {
+        if(stack.size() > 0) {
+            stack.pop();
+        }
+        listAdapter.notifyDataSetChanged();
+        topVal(view);
+    }
+
+    /**
+     * Shows the value at the top of the stack in a textView
+     * @param view
+     */
+    public void topVal(View view) {
+        topEditText = (TextView) findViewById(R.id.topEditText);
+        if(stack.size() > 0) {
+            topEditText.setText(stack.peek().toString());
+        } else {
+            topEditText.setText("");
+        }
+        listAdapter.notifyDataSetChanged();
+    }
+
+    public void clearStack(View view) {
+        stack.clear();
+        listAdapter.notifyDataSetChanged();
+        topVal(view);
     }
 
 
